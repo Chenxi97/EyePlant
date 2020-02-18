e1236e4abf82b0309a8e5bfe80aaacb82aaf2d4f
Chenxi97 <chenxswjtu@gmail.com>
2020-02-18 10:58:13 +0800

    refactor

--- EyePlant/game.py
+++ EyePlant/game.py
@@ -1,82 +1,87 @@
-from configure import *
+import math
+import random
+import time
+import config
 
 
-class drop_object:
-    
-    def __init__(self,speed,radius,object_type):
+class DropObject:
+
+    def __init__(self, speed, radius, object_type):
         self.speed = speed
         self.radius = radius
-        self.x = random.randint(120,520)
-        self.y = UP_LINE
-        #'sunshine':0,'bomb':1,'life':2,
+        self.x = random.randint(120, 520)
+        self.y = config.UP_LINE
+        # 'sunshine':0,'bomb':1,'life':2,
         self.object_type = object_type
-        
-    #向下移动speed个单位
+
+    # 向下移动speed个单位
     def move(self):
-        self.y+=self.speed
-        
-    #检测是否与眼睛重合
-    def is_collision(self,pos_list):
+        self.y += self.speed
+
+    # 检测是否与眼睛重合
+    def is_collision(self, pos_list):
         for pos in pos_list:
-            dis = math.hypot(self.x-pos[0],self.y-pos[1])
+            dis = math.hypot(self.x - pos[0], self.y - pos[1])
             if dis <= self.radius:
                 return True
         return False
-    
-    #检测是否触底
-    def is_bottom(self):
-         return True if self.y>BOTTOM_LINE else False
-        
 
-class Game_manager:
-    
+    # 检测是否触底
+    def is_bottom(self):
+        return True if self.y > config.BOTTOM_LINE else False
+
+
+class GameManager:
+
     def __init__(self):
         self.level = 0
-        self.speed = SPEED_LIST[self.level]
-        self.rate = RATE_LIST[self.level]
-        self.life = LIFE_NUMBER
+        self.speed = config.SPEED_LIST[self.level]
+        self.rate = config.RATE_LIST[self.level]
+        self.life = config.LIFE_NUMBER
         self.sunshine = 0
         self.alive_flag = 1
-        self.growing_flag =0
-        self.idx = IDX_LIST[self.level][0]
-        self.end_idx = IDX_LIST[self.level][1]
+        self.growing_flag = 0
+        self.idx = config.IDX_LIST[self.level][0]
+        self.end_idx = config.IDX_LIST[self.level][1]
         self.finish_flag = 0
 
-    def catch_object(self,object_type):
-        if object_type==0:
-            self.sunshine+=1
-            if self.sunshine>=MAX_SUN:
-                self.growing_flag=1
-                
-        elif object_type==1:
-            self.life-=1
-            if self.life<=0:
-                self.alive_flag=0
+    def catch_object(self, object_type):
+        if object_type == 0:
+            self.sunshine += 1
+            if self.sunshine >= config.MAX_SUN:
+                self.growing_flag = 1
+
+        elif object_type == 1:
+            self.life -= 1
+            if self.life <= 0:
+                self.alive_flag = 0
         else:
-            self.life+=1
-            
-    def upgrade(self,o_manager):
+            self.life += 1
+
+    def upgrade(self, o_manager):
         self.level += 1
-        self.speed = SPEED_LIST[self.level]
-        self.rate = RATE_LIST[self.level]
-        self.idx = IDX_LIST[self.level][0]
-        self.end_idx = IDX_LIST[self.level][1]
+        self.speed = config.SPEED_LIST[self.level]
+        self.rate = config.RATE_LIST[self.level]
+        self.idx = config.IDX_LIST[self.level][0]
+        self.end_idx = config.IDX_LIST[self.level][1]
         self.sunshine = 0
         self.growing_flag = 0
         o_manager.clear()
-        
-class Object_manager:
-    
+
+
+class ObjectManager:
+
     def __init__(self):
-        self.object_list=[]
-        
-    #'sunshine':0,'bomb':1,'life':2,
-    def create_object(self,g_manager):
+        self.object_list = []
+
+    # 'sunshine':0,'bomb':1,'life':2,
+    def create_object(self, g_manager):
         object_type = self.__random_index(g_manager.rate)
-        new_object = drop_object(g_manager.speed,RADIUS,object_type)
+        new_object = DropObject(g_manager.speed, config.RADIUS, object_type)
         self.object_list.append(new_object)
-    
-    def __random_index(self,rate):
+
+    @staticmethod
+    def __random_index(rate):
         # 参数rate为list<int>
         # 返回概率事件的下标索引
         start = 0
@@ -88,36 +93,36 @@ class Object_manager:
             if randnum <= start:
                 break
         return index
- 
-    def update_object(self,pos_list):
-        
-        #删除触底物体
-        while(len(self.object_list)>0 and self.object_list[0].is_bottom()):
-            del(self.object_list[0])
-            
-        #处理碰撞物体
-        ob_list=[]
-        new_object_list=[]
+
+    def update_object(self, pos_list):
+
+        # 删除触底物体
+        while len(self.object_list) > 0 and self.object_list[0].is_bottom():
+            del (self.object_list[0])
+
+        # 处理碰撞物体
+        ob_list = []
+        new_object_list = []
         for ob in self.object_list:
             if ob.is_collision(pos_list):
                 ob_list.append(ob)
             else:
                 new_object_list.append(ob)
         self.object_list = new_object_list
-        
-        #所有物体向下移动
+
+        # 所有物体向下移动
         for ob in self.object_list:
             ob.move()
-            
+
         return ob_list
 
-    
     def clear(self):
         self.object_list.clear()
-    
-class Thread_task:
-    
-    def __init__(self,o_manager):
+
+
+class ThreadTask:
+
+    def __init__(self, o_manager):
         self.__running = True
         self.job = o_manager
 
@@ -127,4 +132,4 @@ class Thread_task:
     def run(self, g_manager):
         while self.__running:
             self.job.create_object(g_manager)
-            time.sleep(TIME_INTERVAL_LIST[g_manager.level])
+            time.sleep(config.TIME_INTERVAL_LIST[g_manager.level])
--- EyePlant/media.py
+++ EyePlant/media.py
@@ -1,55 +1,57 @@
-from configure import *
+import config
+import pygame
+from PIL import Image
 
 
-class Image_dealer:
-    
-    def __init__(self):
-        image_frame = Image.open("image/frame.png").convert('RGBA')
-        image_frame = image_frame.resize((640,480),Image.ANTIALIAS)
-        image_sun = Image.open("image/sun.png").convert('RGBA')
-        image_sun = image_sun.resize((40,40),Image.ANTIALIAS)
-        image_bomb = Image.open("image/bomb.png").convert('RGBA')
-        image_bomb = image_bomb.resize((40,40),Image.ANTIALIAS)
-        image_heart= Image.open("image/heart.png").convert('RGBA')
-        image_heart = image_heart.resize((40,40),Image.ANTIALIAS)
-        image_success = Image.open("image/success.png").convert('RGBA')
-        #image_success = image_success.resize((40,40),Image.ANTIALIAS)
-        image_failure = Image.open("image/failure.png").convert('RGBA')
-        #image_failure  = image_failure.resize((40,40),Image.ANTIALIAS)
-        image_eye = Image.open("image/drogon.png").convert('RGBA')
-        image_eye  = image_eye.resize((10,10),Image.ANTIALIAS)
-        self.img_list = [image_sun,image_bomb,image_heart,image_frame,image_success,
-                         image_failure,image_eye]
-        
-    def paste(self,image,g_manager,pos,type_num):
-        if type_num !=-1:
-            attach_image = self.img_list[type_num]
-        else:
-            attach_image = Image.open("image/tree%dimg/tree%d.png"%(g_manager.level,g_manager.idx)).convert('RGBA')
-            attach_image = attach_image.resize(((int)(attach_image.size[0]/2),(int)(attach_image.size[1]/2)),Image.ANTIALIAS)
-            pos = POS_LIST[g_manager.level]
-            
-        r,g,b,a = attach_image.split()
-        image.paste(attach_image,pos,mask=a)
-        return image
-		
+# 加载图片
+image_frame = Image.open("image/frame.png").convert('RGBA')
+image_frame = image_frame.resize((640, 480), Image.ANTIALIAS)
+image_sun = Image.open("image/sun.png").convert('RGBA')
+image_sun = image_sun.resize((40, 40), Image.ANTIALIAS)
+image_bomb = Image.open("image/bomb.png").convert('RGBA')
+image_bomb = image_bomb.resize((40, 40), Image.ANTIALIAS)
+image_heart = Image.open("image/heart.png").convert('RGBA')
+image_heart = image_heart.resize((40, 40), Image.ANTIALIAS)
+image_success = Image.open("image/success.png").convert('RGBA')
+# image_success = image_success.resize((40,40),Image.ANTIALIAS)
+image_failure = Image.open("image/failure.png").convert('RGBA')
+# image_failure  = image_failure.resize((40,40),Image.ANTIALIAS)
+image_eye = Image.open("image/eye.png").convert('RGBA')
+image_eye = image_eye.resize((10, 10), Image.ANTIALIAS)
+img_list = [image_sun, image_bomb, image_heart, image_frame, image_success,
+            image_failure, image_eye]
 
-class Music:
-    
-    def __init__(self):
-        pygame.mixer.init()
-        sun_sound = pygame.mixer.Sound('sound/water.wav')
-        bomb_sound = pygame.mixer.Sound('sound/bomb.wav')
-        heart_sound = pygame.mixer.Sound('sound/heart.ogg')
-        self.victory_sound = pygame.mixer.Sound('sound/victory.wav')
-        self.failure_sound = pygame.mixer.Sound('sound/failure.ogg')
-        #forge_sound = pygame.mixer.Sound('sound/forge.ogg')
-        #音效列表
-        self.sound_list = [sun_sound,bomb_sound,heart_sound]
-        
-    def main_theme(self):
-        #加载背景音乐文件
-        pygame.mixer.music.load('sound/school.mp3')
-        #播放背景音乐
-        pygame.mixer.music.play(-1, 0.0)
-        pygame.mixer.music.set_volume(0.5)
+# 加载音乐
+pygame.mixer.init()
+sun_sound = pygame.mixer.Sound('sound/water.wav')
+bomb_sound = pygame.mixer.Sound('sound/bomb.wav')
+heart_sound = pygame.mixer.Sound('sound/heart.ogg')
+victory_sound = pygame.mixer.Sound('sound/victory.wav')
+failure_sound = pygame.mixer.Sound('sound/failure.ogg')
+# forge_sound = pygame.mixer.Sound('sound/forge.ogg')
+# 音效列表
+sound_list = [sun_sound, bomb_sound, heart_sound]
+
+
+# 贴图
+def paste(image, g_manager, pos, type_num):
+    if type_num != -1:
+        attach_image = img_list[type_num]
+    else:
+        attach_image = Image.open("image/tree%dimg/tree%d.png" % (g_manager.level, g_manager.idx)).convert('RGBA')
+        attach_image = attach_image.resize((int(attach_image.size[0] / 2), int(attach_image.size[1] / 2)),
+                                           Image.ANTIALIAS)
+        pos = config.POS_LIST[g_manager.level]
+
+    r, g, b, a = attach_image.split()
+    image.paste(attach_image, pos, mask=a)
+    return image
+
+
+# 播放背景音乐
+def main_theme():
+    # 加载文件
+    pygame.mixer.music.load('sound/school.mp3')
+    # 播放
+    pygame.mixer.music.play(-1, 0.0)
+    pygame.mixer.music.set_volume(0.5)
